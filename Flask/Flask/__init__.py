9e2cdee6938e8d697f0c04cc7d19576390de2e23
mfoydl <31784414+mfoydl@users.noreply.github.com>
2019-10-21 14:37:51 -0400

    Base routes
    created entrance and linking connections between loading pages

--- Flask/Flask/__init__.py
+++ Flask/Flask/__init__.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template, url_for, request, jsonify, redirect, j
 from datetime import date
 import sys
 from user import user_api
+from flask_jwt_extended import (
+	JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
 
 app = Flask(__name__)
 app.register_blueprint(user_api)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@jwt_optional
+def home():
+    c_user= get_jwt_identity()
+    if c_user:
+        return render_template('main.html')
+    else:
+        return render_template('login.html')
+    
--- Flask/Flask/user.py
+++ Flask/Flask/user.py
@@ -10,9 +10,33 @@ from flask_jwt_extended import (
 
 user_api = Blueprint('user_api','user_api')
 
+@user_api.route("/createuser",methods=['GET'])
+def createuser():
+    return render_template('createuser.html')
+
 @user_api.route("/adduser",methods=['POST'])
 def addusr():
-    return "addusr"
+    name= request.json.get('username', None)
+	password= request.json.get('password', None)
+	email= request.json.get('email', None)
+
+	client = MongoClient()
+	db= client.users
+    '''
+	json= {"username":name,"password":password,"email":email,"verified":"false"}
+	uid = db.users.insert_one(json)
+	json2= {"email":email,"key":str(uid)}
+	db.verified.insert_one(json2)
+
+	key= "validation key: <"+str(uid.inserted_id)+">\n"
+	url="http://cowzilla.cse356.compas.cs.stonybrook.edu/verify?email={}&key={}".format(email,str(uid.inserted_id))
+	body="Please verify you email with this code:\n "+key+url
+	msg= Message(subject="Verify Email",body=body,sender="ubuntu@wu1.cloud.compas.cs",recipients=[email])
+	mail.send(msg)
+	json_user = {"username":name,"human":"0","wopr":"0","tie":"0","current":"0","games":[],"gamesinfo":[]}
+	db.info.insert_one(json_user)
+    '''
+	return jsonify({"status":"OK"})
 
 @user_api.route("/unverified")
 def unverified():
