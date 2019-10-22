7e0caca7692712154612a8c4f44300a85051442b
mfoydl <31784414+mfoydl@users.noreply.github.com>
2019-10-21 20:38:58 -0400

    Update user.py
    Created full user module

--- Flask/Flask/user.py
+++ Flask/Flask/user.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, jsonify, redirect, json, Blueprint
 from pymongo import MongoClient
 from flask_mail import Mail, Message
+from werkzeug import generate_password_hash, check_password_hash
 from flask_jwt_extended import (
 	JWTManager, jwt_required, create_access_token,
     jwt_refresh_token_required, create_refresh_token,
@@ -16,27 +17,44 @@ def createuser():
 
 @user_api.route("/adduser",methods=['POST'])
 def addusr():
-	name= request.json.get('username', None)
-	password= request.json.get('password', None)
-	email= request.json.get('email', None)
+	try:
+        name= request.json.get('username', None)
 
-	client = MongoClient()
-	db= client.users
-	'''
-	json= {"username":name,"password":password,"email":email,"verified":"false"}
-	uid = db.users.insert_one(json)
-	json2= {"email":email,"key":str(uid)}
-	db.verified.insert_one(json2)
+        password= request.json.get('password', None)
+        hashed_password= generate_password_hash(password)
 
-	key= "validation key: <"+str(uid.inserted_id)+">\n"
-	url="http://cowzilla.cse356.compas.cs.stonybrook.edu/verify?email={}&key={}".format(email,str(uid.inserted_id))
-	body="Please verify you email with this code:\n "+key+url
-	msg= Message(subject="Verify Email",body=body,sender="ubuntu@wu1.cloud.compas.cs",recipients=[email])
-	mail.send(msg)
-	json_user = {"username":name,"human":"0","wopr":"0","tie":"0","current":"0","games":[],"gamesinfo":[]}
-	db.info.insert_one(json_user)
-	'''
-	return jsonify({"status":"OK"})
+        email= request.json.get('email', None)
+
+        client = MongoClient()
+        db= client.naft
+
+        json={
+            "username":name,
+            "email":email,
+            "password":hashed_password,
+            "posts":[]
+            "likes":[]
+            "reposts":[]
+            "following":[]
+            "followers":[]
+            "verified":"false"
+            }
+        #Add new user to database
+        uid = db.users.insert_one(json)
+        json2= {"email":email,"key":str(uid.inserted_id)}
+        db.verified.insert_one(json2)
+
+        # send verification email
+        key= "validation key: <"+str(uid.inserted_id)+">\n"
+        url="http://cowzilla.cse356.compas.cs.stonybrook.edu/verify?email={}&key={}".format(email,str(uid.inserted_id))
+        body="Please verify you email with this code:\n "+key+url
+        msg= Message(subject="Verify Email",body=body,sender="ubuntu@wu1.cloud.compas.cs",recipients=[email])
+        #mail.send(msg)
+
+        # redirect to verification page and return status: OK
+        return make_response(render_template('unverified.html'),jsonify({"status":"OK"}))
+    except Exception, e:
+        return jsonify{"status":"ERROR","error":str(e)} # return status: ERROR if there is an exception
 
 @user_api.route("/unverified")
 def unverified():
@@ -44,17 +62,75 @@ def unverified():
 
 @user_api.route("/verify",methods=['POST','GET'])
 def verify():
-    return "verify"
+    try:
+		email=""
+		key=""
+		if request.method == "POST":
+			email= request.json.get("email", None)
+			key= request.json.get("key", None)
+		elif request.method =="GET":
+			email= request.args.get("email")
+			key=request.args.get("key")
+
+		client= MongoClient()
+		db = client.naft
+
+		vuser= db.verified.find({"email":email})
+		if vuser[0]['key']==key or key=="abracadabra":
+			db.users.update_one({"email":email},{"$set":{"verified":"true"}})
+			return jsonify({"status":"OK"})
+		return jsonify({"status":"ERROR"})
+	except Exception, e:
+		return jsonify({"status":"ERROR","error":str(e)})
 
 @user_api.route("/login",methods=["POST"])
 def login():
-    return 'login'  
+    #check_password_hash(saved,input)
+    try:
+		username = request.json.get('username', None)
+		password = request.json.get('password', None)
 
-@user_api.route('/token/refresh', methods=['POST'])
-#@jwt_refresh_token_required
+		client = MongoClient()
+		db = client.naft
+		users = db.users.find({"username":username})
+
+        if users[0] is None:
+            return jsonify({"status":"ERROR","error":"Username/Password is incorrect"})
+		if(users[0]['verified'] == "false"):
+			return jsonify({"status":"ERROR","error":"You have not verified your account"})
+			
+        if not check_password_hash(users[0]['password'],password):
+            return jsonify({"status":"ERROR","error":"Username/Password is incorrect"})
+
+		access_token = create_access_token(identity=username)
+		refresh_token = create_refresh_token(identity=username)
+
+		resp = jsonify({"status":"OK"})
+		set_access_cookies(resp, access_token)
+		set_refresh_cookies(resp, refresh_token)
+		return resp, 200
+	except Exception, e:
+		return jsonify({"status":"ERROR", "error":str(e)})
+
+@app.route('/token/refresh', methods=['POST'])
+@jwt_refresh_token_required
 def refresh():
-    return "refresh"
+    try:
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
 
-@user_api.route("/logout",methods=["POST"])
+        resp = jsonify({'status':"OK"})
+        set_access_cookies(resp, access_token)
+        return resp, 200
+    except Exception, e:
+		return jsonify({"status":"ERROR","error":str(e)})
+
+@app.route("/logout",methods=["POST"])
 def logout():
-    return "logout"
+	try:
+		resp = jsonify({"status":"OK"})
+		unset_jwt_cookies(resp)
+		return resp, 200
+	except Exception, e:
+		return jsonify({"status":"ERROR","error":str(e)})
+
